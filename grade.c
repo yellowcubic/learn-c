4ced6bfdb42fcd811e88d62ccdd8b98de891ddf9
Worajedt Sitthidumrong <sjedt@3ddaily.com>
2018-03-29 19:03:32 +0700

    start using if-else for 1 criteria

--- grade.c
+++ grade.c
@@ -6,17 +6,21 @@
 #ifdef _WIN32
 #include <tchar.h>
 #else
-  typedef char _TCHAR;
-  #define _tmain main
+typedef char _TCHAR;
+#define _tmain main
 #endif
 
-int _tmain(int argc, _TCHAR* argv[]) 
-{
+int _tmain(int argc, _TCHAR* argv[]) {
 	int score;
 
 	printf("Please enter your score:");
-	scanf("%d",&score);     //scan integer input into score's address
-	printf("Your score is %d",score);
+	scanf("%d", &score); // scan integer input into score's address
+	if (score >= 80) {
+		printf("Your score is %d, you get A", score);
+	}
+	else {
+		printf("Your score is less than 80");
+	}
 
 	getchar();
 	getchar();
